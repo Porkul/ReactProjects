993f2adea9aee1f287efed0ad7a37574e9cc4211
Katrin Porkul <86647070+Porkul@users.noreply.github.com>
2022-03-02 22:47:34 +0200

    Update arrayMap.js

--- ReactExercises/arrayMap.js
+++ ReactExercises/arrayMap.js
@@ -16,3 +16,24 @@
     }
   }
 */
+
+  const users = [
+    { id: 1, name: 'Leanne Graham' },
+    { id: 2, name: 'Ervin Howell' },
+    { id: 3, name: 'Clementine Bauch' },
+    { id: 4, name: 'Patricia Lebsack' }
+  ];
+
+
+  export default class App extends React.Component {
+    render() {
+      const renderUsers = users.map(({id, name}) => {
+        return <li> key={id} name={name}</li>;
+      });
+      return (
+          <ul>
+            {renderUsers}
+          </ul>
+      );
+    }
+  }
