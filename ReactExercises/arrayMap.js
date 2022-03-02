30db2ad809bccb8545c6f6496c44d3703d382555
Katrin Porkul <86647070+Porkul@users.noreply.github.com>
2022-03-02 22:43:54 +0200

    Create arrayMap.js

---
+++ ReactExercises/arrayMap.js
@@ -0,0 +1,18 @@
+/* Map inside <li> </li> users name:
+  const users = [
+    { id: 1, name: 'Leanne Graham' },
+    { id: 2, name: 'Ervin Howell' },
+    { id: 3, name: 'Clementine Bauch' },
+    { id: 4, name: 'Patricia Lebsack' }
+  ];
+
+
+  export default class App extends React.Component {
+    render() {
+      return (
+          <ul>
+          </ul>
+      );
+    }
+  }
+*/
